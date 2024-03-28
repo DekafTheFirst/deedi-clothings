86d7f93c6fc1d7c5262549eda2646f45a49f7067
GamerDTK <dekeji1@gmail.com>
2024-03-28 20:32:01 +0100

    redux connected

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,67 +1,41 @@
 import React from 'react'
 import "./Cart.scss"
 import { DeleteOutlineOutlined } from '@mui/icons-material'
+import {useDispatch, useSelector} from  'react-redux'
+import { removeItem, resetCart } from '../../redux/cartReducer'
 
 const Cart = () => {
-  const data = [
-    {
-        id: 1,
-        img: '/img/products/1.1.jpg',
-        img2: '/img/products/1.2.jpg',
-        title: 'Sweater x White Tee Outfit',
-        isNew: true,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, commodi dolorum iste error perferendis veniam architecto reprehenderit accusamus quia, incidunt voluptas ab molestiae ipsum rem enim consequuntur explicabo? Consequuntur, quisquam.",
-        oldPrice: 19,
-        price: 12,
+  const products = useSelector(state=>state.cart.products)
+  
+  const totalPrice = () => {
+    let total = 0
+    products.forEach(item=> (total += item.quantity*item.price));
+    return total.toFixed(2)
+  }
 
-    },
-    {
-        id: 2,
-        img: '/img/products/2.1.jpg',
-        img2: '/img/products/2.2.jpg',
-        title: 'Casual Outside Outfit',
-        isNew: true,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, commodi dolorum iste error perferendis veniam architecto reprehenderit accusamus quia, incidunt voluptas ab molestiae ipsum rem enim consequuntur explicabo? Consequuntur, quisquam.",
-
-        oldPrice: 19,
-        price: 12,
-
-    },
-    {
-        id: 3,
-        img: '/img/products/3.1.jpg',
-        img2: '/img/products/3.2.jpg',
-        title: 'Casual Outside Outfit',
-        isNew: true,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, commodi dolorum iste error perferendis veniam architecto reprehenderit accusamus quia, incidunt voluptas ab molestiae ipsum rem enim consequuntur explicabo? Consequuntur, quisquam.",
-
-        oldPrice: 19,
-        price: 12,
-
-    },
-  ]
+  const dispatch = useDispatch()
 
 
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
-      {data.map(item=>(
+      {products.map(item=>(
         <div className="item" id={item.id}>
-          <img src={item.img} alt="item" />
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="item" />
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc.substring(0,100)}</p>
-            <div className="price">1 * ${item.price}</div>
+            <div className="price">{item.quantity} * ${item.price}</div>
           </div>
-          <DeleteOutlineOutlined className='delete' />
+          <DeleteOutlineOutlined className='delete' onClick={()=> dispatch(removeItem(item.id))}/>
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
-        <span>$123</span>
+        <span>${totalPrice()}</span>
       </div>
       <button>PROCEED TO CHECKOUT</button>
-      <span className="reset">Reset Cart</span>
+      <span className="reset" onClick={()=> dispatch(resetCart())}>Reset Cart</span>
     </div>
   )
 }
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,77 +1,3 @@
-// import React, { useState } from 'react'
-// import "./Product.scss"
-// import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-// import { Balance, FavoriteBorder } from '@mui/icons-material';
-
-// const Product = () => {
-// const images = [
-//   "/img/products/1.1.jpg",
-//   "/img/products/1.2.jpg"
-// ]
-
-//   const [selectedImage, setSelectedImage] = useState(0)
-//   const [quantity, setQuantity] = useState(0)
-
-//   return (
-//     <div className="product">
-//       <div className="left">
-//         <div className="images">
-//           <img src={images[0]} alt="" onClick={e => setSelectedImage(0)} />
-//           <img src={images[1]} alt="" onClick={e => setSelectedImage(1)} />
-//         </div>
-//         <div className="mainImg">
-//           <img src={images[selectedImage]} alt="" />
-//         </div>
-//       </div>
-//       <div className="right">
-//         <h1 className='title'>Title</h1>
-//         <span className='price'>$199</span>
-//         <p>Lorem ipsum dolor sit,
-//           amet consectetur adipisicing
-//           elit. Dolorum accusamus
-//           laborum porro sunt, impedit
-//           illo eaque maxime sit quaerat
-//           cumque commodi deleniti earum
-//           consequatur ipsam obcaecati hic
-//           saepe quo. Explicabo.
-//         </p>
-//         <div className="quantity">
-//           <button onClick={() => setQuantity(prev => prev === 1 ? 1 : prev - 1)}>-</button>
-//           {quantity}
-//           <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
-//         </div>
-//         <button className="add">
-//           <AddShoppingCartIcon /> ADD TO CART
-//         </button>
-//         <div className="links">
-//           <div className="item">
-//             <FavoriteBorder /> ADD TO WISH LIST
-//           </div>
-//           <div className="item">
-//             <Balance /> ADD TO COMPARE
-//           </div>
-//         </div>
-//         <div className="info">
-//           <span>Vendor: Polo</span>
-//           <span>Product Type: T-Shirt</span>
-//           <span>Tag: T-Shirt, Women, Top</span>
-//         </div>
-//         <hr />
-//         <div className="info">
-//           <span>DESCRIPTION</span>
-//           <hr />
-//           <span>ADDITIONAL INFORMATION</span>
-//           <hr />
-//           <span>FAQ</span>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Product
-
-
 import React from "react";
 import { useState } from "react";
 import "./Product.scss";
@@ -80,12 +6,15 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
   const id = useParams().id
   const [selectedImg, setSelectedImg] = useState("img");
-  const [quantity, setQuantity] = useState(0)
+  const [quantity, setQuantity] = useState(1)
 
+  const dispatch = useDispatch()
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`)
 
 
@@ -140,7 +69,16 @@ const Product = () => {
           </div>
           <button
             className="add"
-
+              onClick={()=>
+                dispatch(addToCart({
+                  id: data.id,
+                  title: data.attributes.title,
+                  desc: data.attributes.desc,
+                  img: data.attributes.img.data.attributes.url,
+                  quantity,
+                  price: data.attributes.price
+                }))
+              }
           >
             <AddShoppingCartIcon /> ADD TO CART
           </button>
